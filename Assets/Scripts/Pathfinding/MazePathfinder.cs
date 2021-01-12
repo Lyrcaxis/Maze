5b293fe050329f8a4d08778cb046ba78e060e11d
Thanasis Galianis <32474602+Lyrcaxis@users.noreply.github.com>
2021-01-12 03:00:38 +0200

    Pathfinder base

---
+++ Assets/Scripts/Pathfinding/MazePathfinder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazePathfinder {
+	public List<Vector2Int> GetMazePath(Vector2Int startPos, Vector2Int targetPos) {
+		var cells = Object.FindObjectOfType<MazeGenerator>().cells;
+
+		var startCell = cells[startPos.x, startPos.y];
+		var endCell = cells[targetPos.x, targetPos.y];
+
+		var checkingCell = startCell;
+		while (checkingCell != endCell) {
+
+		}
+
+		List<Vector2Int> path = new List<Vector2Int>();
+
+		return path;
+	}
+
+	//static List<Vector2Int> 
+
+	static Vector2Int GetPointAtDir(MazeCell c, GridDir dir) {
+		switch (dir) {
+			case GridDir.Left: { return c.pos + Vector2Int.left; }
+			case GridDir.Right: { return c.pos + Vector2Int.right; }
+			case GridDir.Up: { return c.pos + Vector2Int.up; }
+			case GridDir.Down: { return c.pos + Vector2Int.down; }
+			default:
+				throw new UnityException("Wrong direction");
+		}
+	}
+}
